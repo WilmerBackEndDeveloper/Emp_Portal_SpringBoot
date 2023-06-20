1c3ed9da6c588706bbac7b5a3e1d4ca8bd98774e
Ina <qureshi@wilmertech.com>
2023-06-20 11:40:41 +0530

    Updated Dependency with Configuration File to connect DB

---
+++ Empportal/src/main/java/io/aidenai/empportal/Empportal/EmpportalApplication.java
@@ -0,0 +1,13 @@
+package io.aidenai.empportal.Empportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpportalApplication.class, args);
+	}
+
+}
---
+++ Empportal/src/test/java/io/aidenai/empportal/Empportal/EmpportalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aidenai.empportal.Empportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
